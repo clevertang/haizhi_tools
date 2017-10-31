ad2b8f86171c47549a31986ca0da6d8240ea2dd8
tangxin <961577196@qq.com>
2017-10-31 11:04:34 +0800

    工商新注册企业mysql表自动更细

--- my_tools/start.sh
+++ my_tools/start.sh
@@ -1,2 +1,1 @@
-nohup /home/work/env/python-2.7/bin/python mongo_count.py &>mongo_count.log &
-nohup /home/work/env/python-2.7/bin/python huoche_excel.py &>huoche_excel.log &
+nohup /home/work/env/python-2.7/bin/python enterprise_mysql.py &>enterprise_mysql.log &
