bf90c8d29a2d74d23b7c1cd62bdcf7c6ffda1d7e
tangxin <961577196@qq.com>
2017-11-10 20:16:24 +0800

    bid 导出脚本

---
+++ my_tools/bid_zip.py
@@ -0,0 +1,74 @@
+# -*- coding:UTF-8 -*-
+# !/usr/bin/env python
+"""
+-------------------------------------------------
+   File Name：     bid_zip
+   Description :
+   Author :       tangxin
+   date：          2017/11/10
+-------------------------------------------------
+   Change Activity:
+                   2017/11/10:
+-------------------------------------------------
+"""
+
+import json
+import sys
+
+import datetime
+import pymongo
+import zipfile
+
+from my_tools import my_email
+
+sys.path.append("..")
+sys.path.append("../..")
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+class GsCredit:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def start():
+        time = datetime.date.today()
+        step_time = datetime.timedelta(days=1)
+        time = time - step_time
+        time1 = time - step_time
+        tables = [
+            "bid_detail"
+        ]
+        db_save = pymongo.MongoClient('172.16.215.16', 40042)['app_data']
+        db_save.authenticate('work', 'haizhi')
+        filename = '{0}.txt'.format('bid_detail--' + str(time) + '--' + str(time1))
+        for table in tables:
+            _file = open(filename, 'w')
+            cursor = db_save[table].find(
+                {"_in_time": {"$gte": "{}".format(time1), "$lte": "{}".format(time)}})
+            for element in cursor:
+                try:
+                    data = {
+                        "bid_content": element.get("bid_content", ""),
+                        "_src": element.get("_src", ""),
+                        "title": element.get("title", ""),
+                        "province": element.get("province", "")
+                    }
+                    _file.write(json.dumps(data, ensure_ascii=False) + "\n")
+                except Exception, e:
+                    print e.message
+            _file.close()
+        z = zipfile.ZipFile(filename.replace("txt", "zip"), 'w', zipfile.ZIP_DEFLATED)
+        z.write(filename)
+        z.close()
+        # my_email.qq_send("招标信息-{}".format(date), "招标信息-{}".format(date), filename.replace("txt", "zip"),
+        #                  "tangxin@haizhi.com")
+        print filename
+        my_email.hz_send("招标信息-{}".format(date), filename.replace("txt", "zip"), "tangxin@haizhi.com")
+
+
+if __name__ == "__main__":
+    date = datetime.date.today()
+    worker = GsCredit()
+    worker.start()
--- my_tools/my_email.py
+++ my_tools/my_email.py
@@ -19,7 +19,7 @@ hubo = my_setting.email_setting["toaddress2"]
 _cc = "zhangjun@haizhi.com"
 
 
-def send(topic, name, _to):
+def hz_send(topic, name, _to):
     msg = MIMEMultipart()
     msg["Accept-Language"] = "zh-CN"
     msg["Accept-Charset"] = "ISO-8859-1,utf-8"
@@ -46,6 +46,34 @@ def send(topic, name, _to):
         print "Falied,%s" % e
 
 
+def qq_send(topic, subject, filename, _to):
+    msg = MIMEMultipart()
+    msg["Accept-Language"] = "zh-CN"
+    msg["Accept-Charset"] = "ISO-8859-1,utf-8"
+    msg["Subject"] = subject
+    msg["From"] = "961577196@qq.com"
+    msg["To"] = _to
+    msg["Cc"] = _cc
+    part = MIMEText(topic, "utf-8")
+    msg.attach(part)
+
+    # ---这是附件部分---
+    # xlsx类型附件
+    part = MIMEApplication(open(filename, 'rb').read())
+    part.add_header('Content-Disposition', 'attachment', filename=filename.encode("gb2312"))
+    msg.attach(part)
+
+    try:
+        s = smtplib.SMTP_SSL(my_setting.email_setting_qq["server"], 465)
+        s.login(my_setting.email_setting_qq["addr"], my_setting.email_setting_qq["passwd"])
+        s.sendmail("961577196@qq.com", _to.split(","), msg.as_string())
+        s.quit()
+        print "Success!"
+    except smtplib.SMTPException, e:
+        print "Falied,%s" % e
+
+
 if __name__ == "__main__":
-    send("火车采集器统计情况", '火车采集器{}数据统计.xls'.format(datetime.date.today()).decode("utf-8"), hubo)
+    pass
+    # send("火车采集器统计情况", '火车采集器{}数据统计.xls'.format(datetime.date.today()).decode("utf-8"), hubo)
     # send("招行站点统计情况", "result.xls", youfeng)
