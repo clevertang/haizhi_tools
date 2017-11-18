26ac691a5d933302855cfd51e375b71d38e33d73
tangxin <tangxin@haizhi.com>
2017-11-18 14:27:44 +0800

    tangxin

--- my_tools/mongo_lanzhou_project.py
+++ my_tools/mongo_lanzhou_project.py
@@ -61,16 +61,12 @@ def ktgg(province):
     total = 0
     cur2 = db["court_ktgg"].find({})
     print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-    aaa = 0
     for item in cur2:
-        aaa += 1
-
         province2 = item.get("province", "")
         if item.get("province", "") is None:
             province2 = ""
         if province in province2:
             total += 1
-        print aaa
     print time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
     return total
 
